873ff01c628dd91cf874a5f485a8059b01e17b4f
Ofir Gilad <ofir.gilad9631@gmail.com>
2024-02-02 23:38:51 +0200

    added readme

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from state import State
 from simulator import Simulator
 
 
-def main():
-    data_file_path = "./input.txt"
+def run(data_filepath: str):
     parser = Parser()
-    environment_data = parser.parse_data(data_filepath=data_file_path)
+    environment_data = parser.parse_data(data_filepath=data_filepath)
     # print(environment_data)
     initial_state = State(environment_data=environment_data)
     # print(initial_state.adjacency_matrix)
     simulator = Simulator(initial_state=initial_state)
     simulator.run()
+
+
+def main():
+    # TODO: Fill the data_filepath parameter
+    data_filepath = "./input.txt"
+    run(data_filepath=data_filepath)
 
 
 if __name__ == '__main__':
